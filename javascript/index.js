87bbe375cec5e924e77428430a6279b019854089
laurabatlle <laura.batlle007@gmail.com>
2023-02-06 23:03:33 +0100

    added tokyo city

--- javascript/index.js
+++ javascript/index.js
@@ -22,6 +22,20 @@ function updateTime() {
       "h:mm:ss [<small>]A[</small>]"
     );
   }
+
+  //Tokyo
+
+  let tokyoElement = document.querySelector("#tokyo");
+  if (tokyoElement) {
+    let tokyoDateElement = tokyoElement.querySelector(".date");
+    let tokyoTimeElement = tokyoElement.querySelector(".time");
+    let tokyoTime = moment().tz("Asia/Tokyo");
+
+    tokyoDateElement.innerHTML = tokyoTime.format("MMMM	Do YYYY");
+    tokyoTimeElement.innerHTML = tokyoTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
 }
 function updateCity(event) {
   let cityTimeZone = event.target.value;
@@ -41,6 +55,7 @@ function updateCity(event) {
     "A"
   )}</small></div>
   </div>
+  <a href="/"> Home page </a>
   `;
 }
 updateTime();
