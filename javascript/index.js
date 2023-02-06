90b581d45b2af396bbd2c5fd4065154d2866a853
laurabatlle <laura.batlle007@gmail.com>
2023-02-06 22:43:55 +0100

    fixed name and class

--- javascript/index.js
+++ javascript/index.js
@@ -25,11 +25,11 @@ function updateTime() {
 }
 function updateCity(event) {
   let cityTimeZone = event.target.value;
-  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  let cityName = cityTimeZone.split("/")[1];
   let cityTime = moment().tz(cityTimeZone);
   let citiesElement = document.querySelector("#cities");
   citiesElement.innerHTML = `
-  <div class="city">
+  <div class="cities">
     <div>
       <h2>${cityName}</h2>
       <div class="date">${cityTime.format("MMMM	Do YYYY")}</div>
