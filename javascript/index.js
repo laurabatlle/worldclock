3fc5667c8d8bda65650158017ae62a57c8cc6b7a
laurabatlle <laura.batlle007@gmail.com>
2023-02-06 21:44:02 +0100

    added time and date for Barcelona and New York

--- javascript/index.js
+++ javascript/index.js
@@ -7,5 +7,36 @@ function showTime(event) {
     alert(`It is ${currentTime} in ${event.target.value}`);
   }
 }
+
+function updateTime() {
+  // Barcelona
+  let barcelonaElement = document.querySelector("#barcelona");
+  if (barcelonaElement) {
+    let barcelonaDateElement = barcelonaElement.querySelector(".date");
+    let barcelonaTimeElement = barcelonaElement.querySelector(".time");
+    let barcelonaTime = moment().tz("Europe/Barcelona");
+
+    barcelonaDateElement.innerHTML = barcelonaTime.format("MMMM	Do YYYY");
+    barcelonaTimeElement.innerHTML = barcelonaTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
+
+  // New York
+  let newYorkElement = document.querySelector("#new-york");
+  if (newYorkElement) {
+    let newYorkDateElement = newYorkElement.querySelector(".date");
+    let newYorkTimeElement = newYorkElement.querySelector(".time");
+    let newYorkTime = moment().tz("America/New_York");
+
+    newYorkDateElement.innerHTML = newYorkTime.format("MMMM	Do YYYY");
+    newYorkTimeElement.innerHTML = newYorkTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
+}
+updateTime();
+setInterval(updateTime, 1000);
+
 let selectCity = document.querySelector("#cities");
 selectCity.addEventListener("change", showTime);
